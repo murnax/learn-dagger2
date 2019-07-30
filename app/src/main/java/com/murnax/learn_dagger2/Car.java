19da03fde168262a0b119c9874734e12e773e27a
Suratin Pattanawongthai <suratin.p@gobike.asia>
2019-07-30 19:40:15 +0700

    add constructor injection to car, engine and wheels classes

--- app/src/main/java/com/murnax/learn_dagger2/Car.java
+++ app/src/main/java/com/murnax/learn_dagger2/Car.java
@@ -2,12 +2,15 @@ package com.murnax.learn_dagger2;
 
 import android.util.Log;
 
+import javax.inject.Inject;
+
 public class Car {
     private static final String TAG = "Car";
 
     private Engine engine;
     private Wheels wheels;
 
+    @Inject
     public Car(Engine engine, Wheels wheels) {
         this.engine = engine;
         this.wheels =  wheels;
--- app/src/main/java/com/murnax/learn_dagger2/Engine.java
+++ app/src/main/java/com/murnax/learn_dagger2/Engine.java
@@ -1,4 +1,10 @@
 package com.murnax.learn_dagger2;
 
+import javax.inject.Inject;
+
 public class Engine {
+    @Inject
+    public Engine() {
+
+    }
 }
