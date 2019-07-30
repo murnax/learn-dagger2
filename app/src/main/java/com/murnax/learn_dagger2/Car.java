0e2aa418ed633f734401ddf7612002c2a5568579
Suratin Pattanawongthai <suratin.p@gobike.asia>
2019-07-30 19:30:35 +0700

    add detail to Car class

--- app/src/main/java/com/murnax/learn_dagger2/Car.java
+++ app/src/main/java/com/murnax/learn_dagger2/Car.java
@@ -1,4 +1,19 @@
 package com.murnax.learn_dagger2;
 
+import android.util.Log;
+
 public class Car {
+    private static final String TAG = "Car";
+
+    private Engine engine;
+    private Wheels wheels;
+
+    public Car(Engine engine, Wheels wheels) {
+        this.engine = engine;
+        this.wheels =  wheels;
+    }
+
+    public void drive() {
+        Log.d(TAG, "drive...");
+    }
 }
