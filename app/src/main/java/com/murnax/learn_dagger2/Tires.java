13ae1501e16d1a7e25a0d9affbf3653da713dc78
Suratin Pattanawongthai <suratin.elec@gmail.com>
2019-07-31 08:01:40 +0700

    Update Wheels class and create two new classes Rims and Tires and assume that we do not have access to annotate constructor of all these classes

---
+++ app/src/main/java/com/murnax/learn_dagger2/Tires.java
@@ -0,0 +1,11 @@
+package com.murnax.learn_dagger2;
+
+import android.util.Log;
+
+public class Tires {
+    private static final String TAG = "Car";
+
+    public void inflate() {
+        Log.d(TAG, "tires inflated");
+    }
+}
--- app/src/main/java/com/murnax/learn_dagger2/Wheels.java
+++ app/src/main/java/com/murnax/learn_dagger2/Wheels.java
@@ -1,10 +1,12 @@
 package com.murnax.learn_dagger2;
 
-import javax.inject.Inject;
-
 public class Wheels {
-    @Inject
-    public Wheels() {
 
+    private Rims rims;
+    private Tires tires;
+
+    public Wheels(Rims rims, Tires tires) {
+        this.rims = rims;
+        this.tires = tires;
     }
 }
