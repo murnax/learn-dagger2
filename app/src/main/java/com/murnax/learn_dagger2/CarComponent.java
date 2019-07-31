8c74e77b1099a410ecea7c88df37bba1c113805f
Suratin Pattanawongthai <suratin.elec@gmail.com>
2019-07-31 08:30:43 +0700

    create DieselEngineModule and switch module in CarComponent to use this new engine module

--- app/src/main/java/com/murnax/learn_dagger2/CarComponent.java
+++ app/src/main/java/com/murnax/learn_dagger2/CarComponent.java
@@ -2,9 +2,10 @@ package com.murnax.learn_dagger2;
 
 import dagger.Component;
 
-@Component(modules = { WheelsModule.class, PetrolEngineModule.class })
+@Component(modules = { WheelsModule.class, DieselEngineModule.class })
 public interface CarComponent {
     Car getCar();
 
     void inject(MainActivity mainActivity);
 }
+ 
---
+++ app/src/main/java/com/murnax/learn_dagger2/DieselEngineModule.java
@@ -0,0 +1,11 @@
+package com.murnax.learn_dagger2;
+
+import dagger.Binds;
+import dagger.Module;
+
+@Module
+public abstract class DieselEngineModule {
+
+    @Binds
+    abstract Engine bindEngine(DieselEngine engine);
+}
