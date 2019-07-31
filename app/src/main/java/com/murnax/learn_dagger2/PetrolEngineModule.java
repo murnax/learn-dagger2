eabd630f35d80b0313f94c0d8b9c02531d4a1b54
Suratin Pattanawongthai <suratin.elec@gmail.com>
2019-07-31 08:28:32 +0700

    change from @Provides to @Binds because if we only need to binds implementation to the interface, @Binds annotation will yield a better performance.

--- app/src/main/java/com/murnax/learn_dagger2/PetrolEngineModule.java
+++ app/src/main/java/com/murnax/learn_dagger2/PetrolEngineModule.java
@@ -1,12 +1,11 @@
 package com.murnax.learn_dagger2;
 
+import dagger.Binds;
 import dagger.Module;
-import dagger.Provides;
 
 @Module
-public class PetrolEngineModule {
-    @Provides
-    Engine provideEngine(PetrolEngine engine) {
-        return engine;
-    }
+public abstract class PetrolEngineModule {
+
+    @Binds
+    abstract Engine bindEngine(PetrolEngine engine);
 }
